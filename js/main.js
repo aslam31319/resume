46f7ff21bff3af4e1e2f2559f4158977276a2951
aslucmd <aslam31319@gmail.com>
2022-09-10 13:19:06 +0530

    changes for the theme selector

--- js/main.js
+++ js/main.js
@@ -7,6 +7,10 @@ function myFunction_get(propertyName) {
 }
 
 function myFunction_set_dark() {
+    var lightButton = document.getElementById('button--light');
+    var darkButton = document.getElementById('button--dark');
+    darkButton.setAttribute('hidden', true);
+    lightButton.removeAttribute('hidden');
     r.style.setProperty('--mainTextColor', myFunction_get('--mainTextColor-dark'));
     r.style.setProperty('--secondaryTextColor', myFunction_get('--secondaryTextColor-dark'));
     r.style.setProperty('--mainLinkColor', myFunction_get('--mainLinkColor-dark'));
@@ -15,6 +19,10 @@ function myFunction_set_dark() {
 }
 
 function myFunction_set_light() {
+    var lightButton = document.getElementById('button--light');
+    var darkButton = document.getElementById('button--dark');
+    lightButton.setAttribute('hidden', true);
+    darkButton.removeAttribute('hidden');
     r.style.setProperty('--mainTextColor', myFunction_get('--mainTextColor-light'));
     r.style.setProperty('--secondaryTextColor', myFunction_get('--secondaryTextColor-light'));
     r.style.setProperty('--mainLinkColor', myFunction_get('--mainLinkColor-light'));
